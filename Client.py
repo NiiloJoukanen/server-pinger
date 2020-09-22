8e5fd4e69ea672e0773fc539c0ea41f8a391b9bd
Niilo Joukanen <niilo.joukanen@gmail.com>
2020-09-22 18:02:17 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,16 +2,16 @@ import socket
 import time
 
 def main():
-    hostname = "IP HERE"
-    port = 1234
+    hostname = "IP HERE"  # set correct IP here
+    port = 1234  # set correct port
 
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                s.connect((hostname, port))
-                print(f"Connected to {hostname}")
-                time.sleep(10)
-            except Exception as e:
+                s.connect((hostname, port))  # connect to server
+                print(f"Connected to {hostname}")  # output
+                time.sleep(10)  # wait 10 seconds
+            except Exception as e:  # unable to connect to server
                 print(f"Error while connecting to {hostname}")
                 print(e)
 
