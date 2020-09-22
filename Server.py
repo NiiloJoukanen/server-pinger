5e7af6c71238f9ff8f25d87442d9f906e3c29eaa
Niilo Joukanen <niilo.joukanen@gmail.com>
2020-09-22 18:01:03 +0300

    Update Server.py

--- Server.py
+++ Server.py
@@ -6,10 +6,11 @@ from datetime import datetime
 def connection_timer():
     while True:
         time.sleep(1)
-        current_time = time.time()
-        last_connection_interval = int(current_time - last_connection_time)
-
-        if last_connection_interval > 4 and last_connection_interval % 5 == 0:
+        current_time = time.time()  # get current time
+        last_connection_interval = int(current_time - last_connection_time)  # calculate interval between last connection
+        
+        if last_connection_interval >= 60 and last_connection_interval % 60 == 0:
+            # last connection was over a minute ago
             current_time = datetime.now().strftime("%H:%M:%S")
             print(f"[{current_time}]: Last connection was {last_connection_interval} seconds ago!")
         
@@ -20,17 +21,18 @@ def main():
     port = 1234
     last_connection_time = time.time()
 
+    # start server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((hostname, port))
         s.listen(1)
-
+        
         while True:
-            conn, addr = s.accept()
-            connection_interval = int(time.time() - last_connection_time)
-            print(f"Connection from {addr[0]} | Connection interval was {connection_interval} seconds")
-            conn.close()
-            last_connection_time = time.time()
+            conn, addr = s.accept()  # accept all connections
+            connection_interval = int(time.time() - last_connection_time)  # calculate interval from the last connection
+            print(f"Connection from {addr[0]} | Connection interval was {connection_interval} seconds")  # output
+            conn.close()  # close connection
+            last_connection_time = time.time()  # store connection time
 
 if __name__ == "__main__":
     threading.Thread(target=connection_timer).start()
